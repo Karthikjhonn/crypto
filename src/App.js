9876a99e47ca41f6997fef69cee20f4b025ed15c
karthikjohnn <karthickyamkk2001@gmail.com>
2023-10-13 23:39:31 +0530

    first commite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Market from './features/home/Market';
+import LargeDevicesNav from './features/nav/LargeDevicesNav';
+import Nav from './features/nav/Nav';
+import NavOverLay from './features/nav/NavOverLay';
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section >
+      {/* small device Navigation  */}
+      <header className='lg:hidden sticky top-0 z-[50] shadow-xl'>
+        <Nav />
+        <NavOverLay />
       </header>
-    </div>
+      <section className='flex min-h-screen h-full'>
+        {/* large device Navigation */}
+        <header className='w-1/4 max-w-[275px]  hidden lg:block'>
+          <LargeDevicesNav />
+        </header>
+        <Market />
+      </section>
+    </section>
   );
 }
 
