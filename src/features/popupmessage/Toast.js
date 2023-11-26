ae87e8f57505b90ad36438bede1b45ee3473b0e4
karthikjohnn <karthickyamkk2001@gmail.com>
2023-11-26 22:41:01 +0530

    none

--- src/features/popupmessage/Toast.js
+++ src/features/popupmessage/Toast.js
@@ -13,6 +13,7 @@ function Toast({ message ,openModel ,closeModel}) {
                 settoasthide('bg-white shadow-2xl text-[14px] text-center w-[92%] max-w-lg font-medium rounded-sm px-2.5 py-1.5 text-black inline-block absolute -bottom-12 left-1/2 -translate-x-1/2  transition-all duration-150 ease-linear capitalize ');
             }, openModel);
             setTimeout(() => {
+                console.log("none");
                 setdisplaynone('bg-black/50 fixed top-0 left-0 w-full h-screen z-[700] overflow-hidden transition-all duration-150 ease-linear hidden')
             }, closeModel);
         };
